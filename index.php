04c57b4bef4c27b17d9f6d258da45a9704b4d3a1
veroandi <veroandi.wcs@gmail.com>
2017-03-31 04:45:52 +0200

    include footer and header

--- index.php
+++ index.php
@@ -49,14 +49,18 @@ $result = json_decode($return, true);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bienvenue sur ODL</title>
+    <link rel="stylesheet" href="public/css/stylesheet.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
           crossorigin="anonymous">
 </head>
 <body>
+<?php include "inc/header.php"?>
 <?php include "inc/form.php"?>
 <?php include "inc/list.php"?>
+<?php include "inc/footer.php"?>
 
 <script src="http://code.jquery.com/jquery-3.2.1.min.js"
         integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
